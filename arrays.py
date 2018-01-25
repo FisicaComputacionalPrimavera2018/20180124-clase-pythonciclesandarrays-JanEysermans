120ef171ca187d653b11bdff7ae90f104ce8b86d
Jan Eysermans <216570090@control.fcfm.buap.mx>
2018-01-24 20:15:16 -0600

    first commit

---
+++ arrays.py
@@ -0,0 +1,36 @@
+import math
+
+# arrays
+# arrays can hold any type (strings, ints, floats, ...)
+
+x = 5.5
+y = 4
+z = 0
+s = "test"
+
+vector1 = [x, y, z, s, 222]
+vector2 = [5.5, 4, 0, "test"]
+
+print vector1
+print vector2
+
+# Print all the elements of an array
+print vector1[0]
+print vector1[1]
+print vector1[2]
+print vector1[3]
+print vector1[4]
+
+
+# loop over arrays
+for arrayElement in vector1:
+
+	print arrayElement
+
+
+# length of array
+print "---------------------"
+print len(vector1)
+print len(vector2)
+
+
---
+++ variables.py
@@ -0,0 +1,25 @@
+# import math library to use mathematical funcions (e.g. ceil, floor)
+# see: https://docs.python.org/2/library/math.html
+import math
+
+# this is a comment
+
+# lets start with VARIABLES
+
+# define some variables: strings, float, ints
+x = 1 # integer
+y = "test" # string
+z = str(x) + "-" + y # add integer to strings by using the str() function
+
+print x,y,z
+
+
+a = 5.2 # float
+b = 2 # int
+c = int(a) + b # convert a to integer
+
+
+print int(6.6), math.ceil(6.1), math.floor(6.5)
+
+print a,b,c
+
